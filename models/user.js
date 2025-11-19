7688bcd10226202bf1a8af686d7b2ff5cc7e2550
butch3r <144351159+bbbutch3rrr@users.noreply.github.com>
2025-11-20 04:49:58 +0800

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
-// Simple User Schema for Authentication
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
-    password: { type: String, required: true } // In a real app, hash this!
-});
+    password: { type: String, required: true }});
 
 module.exports = mongoose.model('User', userSchema);
