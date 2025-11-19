3a429324633b0abd29f30514784742e0ec556e5f
butch3r <144351159+bbbutch3rrr@users.noreply.github.com>
2025-11-20 04:49:25 +0800

    Update task.js

--- models/task.js
+++ models/task.js
@@ -1,15 +1,11 @@
 const mongoose = require('mongoose');
 
-// Task Schema containing title, description, and status
 const taskSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String },
-    status: { 
-        type: String, 
-        enum: ['Pending', 'In Progress', 'Completed'], 
-        default: 'Pending' 
-    },
-    createdAt: { type: Date, default: Date.now }
-});
+    status: { type: String, 
+             enum: ['Pending', 'In Progress', 'Completed'], 
+             default: 'Pending' },
+    createdAt: { type: Date, default: Date.now }});
 
 module.exports = mongoose.model('Task', taskSchema);
